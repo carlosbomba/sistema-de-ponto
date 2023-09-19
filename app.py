4493958d83352c61eeb1301533b65ea03213c01b
carlosbomba <carlostorricomendoza@gmail.com>
2023-09-19 17:53:06 -0300

    criando segunda funcao e atualizando funcao inicial

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from datetime import datetime, timezone
+
 
 app = Flask(__name__)
 
-@app.route("/run-query", methods=["POST"])
-def ponto_func():
-    return 'hello'
+@app.route("/ponto-adicao", methods=["POST"])
+def ponto_adicao():
+    tempo = datetime.now()
+    # usuario = request.json.get('usuario')
+    return "Ponto cadastrado com sucesso!"
+
+@app.route("/busca-ponto", methods=["POST"])
+def busca_ponto():
+    tempo = datetime.now()
+    command = request.json.get('command')
+    return str(tempo)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5001, debug=True)
